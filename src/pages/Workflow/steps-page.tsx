be0513bff01a054987e4aa87c8ea5cf996ccc81e
Hojat Nasiri <hnasiri1997@gmail.com>
2025-04-26 13:40:33 +0330

    update

--- src/pages/Workflow/steps-page.tsx
+++ src/pages/Workflow/steps-page.tsx
@@ -511,13 +511,14 @@ function Dashboard() {
               <Label>فرم های نمایشی</Label>
 
               <select
+                multiple
                 value={formhayeNamayeshi}
                 onChange={(e) => {
                   const options = [...e.target.selectedOptions];
                   const values = options.map((option) => +option.value);
                   setFormhayeNamayeshi(values);
                 }}
-                multiple
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 {steps.map((step) => {
                   if (step.relatedForm) {
